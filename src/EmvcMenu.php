5765ec7e7193866de19e7a039a347ac4ed3bf952
Rudy Mas <rudy.mas@rudymas.be>
2018-10-06 18:12:06 +0200

    Code updated by removing depricated functions

--- src/EmvcMenu.php
+++ src/EmvcMenu.php
@@ -8,7 +8,7 @@ namespace EasyMVC\Menu;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.0.2.2
+ * @version     0.1.0.3
  * @package     EasyMVC\Menu
  */
 class EmvcMenu
---
+++ version.md
@@ -0,0 +1,9 @@
+### Version of the package
+
+version: 2.0.1.16
+
+### Verion's of the classes in the package
+
+Menu: 2.0.1.16  
+BootstrapMenu: 0.0.2.2  
+EmvcMenu: 0.1.0.3
